2d075773fd48ca54caf6590990e116806005d4a7
wescratty <wescratty@gmail.com>
2016-02-07 12:36:01 -0700

    added export csv method

--- platforms/browser/www/index_helper.js
+++ platforms/browser/www/index_helper.js
@@ -1,9 +1,14 @@
 
 var refreshIntervalId = null;
 var myLiveChart;
+var dataOutArray = [];
+
+
 document.addEventListener("deviceready", onDeviceReady, false);
 // device APIs are available
 //
+
+
 function onDeviceReady() {
     var canvas = document.getElementById('updating-chart'),
     ctx = canvas.getContext('2d'),
@@ -24,40 +29,29 @@ function onDeviceReady() {
     
 }
 
+
+
 function startLocationPoints(){
     if (refreshIntervalId == null){
         refreshIntervalId = setInterval(getNew, 1000);
     }else{
         clearInterval(refreshIntervalId);
         refreshIntervalId = null;
-        
     }
 }
+
+
 
 function getNew(){
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
 }
 
+
+
 function onSuccess(position) {
-    var speed = position.coords.speed;
-    speed = Math.random() *20;
-    if (speed<0) {
-        speed = 0;
-    };
     
-    myLiveChart.addData([speed],Math.floor((Date.now()-1454739000000)/4000 ));//position.coords.speed,Math.floor(Date.now() / 1000)
-    
-    function sendCSV(){
-        
-        var csvContent = "data:text/csv;charset=utf-8,";
-        data.forEach(function(infoArray, index){
-                     
-                     dataString = infoArray.join(",");
-                     csvContent += index < data.length ? dataString+ "\n" : dataString;
-                     
-                     }); }
-    
-    
+    addDataToChart(position);
+
     var element = document.getElementById('geolocation');
     element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
     'Longitude: '          + position.coords.longitude             + '<br />' +
@@ -68,12 +62,103 @@ function onSuccess(position) {
     'Speed: '              + position.coords.speed                 + '<br />' +
     'Timestamp: '          + position.timestamp                    + '<br />';
 }
-// onError Callback receives a PositionError object
-//
+
+function addDataToChart(position){
+    var speed = position.coords.speed; 
+    var time = Math.floor((Date.now()-1454739000000)/4000 );
+    speed = Math.random() * 20;  // intercepts and gives something to 
+
+    
+    if (speed==0||speed<0) {
+        speed = Math.random() * 20;  // intercepts and gives something to
+    };
+    
+    myLiveChart.addData([speed],time);//position.coords.speed,Math.floor(Date.now() / 1000)
+    // dataOutArray.push(speed+'\n');
+    dataOutArray.push(speed+', '+time+'\n');
+
+}
+
+
+function sendCSV(){
+
+    
+    // var data = $.parseJSON( txt ).dataOutArray;
+
+    var $table = $( "<table></table>" );
+
+    for ( var i = 0; i < dataOutArray.length; i++ ) {
+        var dat = dataOutArray[i];
+        var $line = $( "<tr></tr>" );
+        $line.append( $( "<td></td>" ).html( dat) );
+        $table.append( $line );
+    }
+
+    $table.appendTo( $( "#tableDiv" ) );
+
+
+    var csvFile = null,
+  makeTextFile = function (csv) {
+    var data = new Blob([csv], {type: 'text/csv'});
+
+    // If we are replacing a previously generated file we need to
+    // manually revoke the object URL to avoid memory leaks.
+    if (csvFile !== null) {
+      window.URL.revokeObjectURL(csvFile);
+    }
+
+    csvFile = window.URL.createObjectURL(data);
+
+    return csvFile;
+  };
+
+
+  var create = document.getElementById('create'),
+    textbox = document.getElementById('tableDiv');
+
+  create.addEventListener('click', function () {
+    var link = document.getElementById('downloadlink');
+    link.href = makeTextFile(dataOutArray.toString());
+    link.style.display = 'block';
+  }, false);
+
+
+
+
+// blob = new Blob([dataOutArray], { type: 'text/csv' }); //new way
+// var csvUrl = URL.createObjectURL(blob);
+
+// $(this)
+// .attr({
+//     'download': filename,
+//     'href': csvUrl
+// });
+
+
+
+ // document.getElementById("myDiv").innerHTML = $table;
+ //    var csvContent = "$table:text/csv;charset=utf-8,";
+
+ //    var encodedUri = encodeURI(csvContent);
+ //    var link = document.createElement("a");
+ //    link.setAttribute("href", encodedUri);
+ //    link.setAttribute("download", "my_data.csv");
+
+ //    link.click(); // This will download the data file named "my_data.csv".
+
+
+  
+}
+
+
+
+
 function onError(error) {
     alert('code: '    + error.code    + '\n' +
           'message: ' + error.message + '\n');
 }
+
+
 function clickedButton(){
     window.location = 'signIn.html'
 
