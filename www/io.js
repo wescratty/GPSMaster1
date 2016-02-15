77aa7643c066241f00a30d95e2898f83329ce627
Brian_Trethewey <brian.trethewey@umontana.edu>
2016-02-14 21:17:19 -0700

    test template

--- www/io.js
+++ www/io.js
@@ -32,6 +32,7 @@ function readTextFile(file)
             if(rawFile.status === 200 || rawFile.status == 0)
             {
                 var allText = rawFile.responseText;
+
                 alert(allText);
             }
         }
