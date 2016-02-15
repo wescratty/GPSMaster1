fb145fc4d89a77e23f56640b4e443ee00642d01f
Brian_Trethewey <brian.trethewey@umontana.edu>
2016-02-14 18:25:18 -0700

    template of file read

--- www/io.js
+++ www/io.js
@@ -6,9 +6,38 @@ function savetoCSV(Filename){
 }
 
 function readinCSV(Filename){
+    function win(file) {
+        var reader = new FileReader();
+        reader.onloadend = function (evt) {
+            console.log("read success");
+            console.log(new Uint8Array(evt.target.result));
+        };
+        reader.readAsArrayBuffer(file);
+    };
+
+    var fail = function (error) {
+        console.log(error.code);
+    };
 
 }
-
+// template I found online  want to see how this works
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
 function loadCSV(){
 
 }
