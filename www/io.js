9292477c6f69c1a33adc68310b1e88d1b39c461b
Brian_Trethewey <brian.trethewey@umontana.edu>
2016-02-14 17:06:47 -0700

    add points method

--- www/io.js
+++ www/io.js
@@ -1,7 +1,7 @@
 //handles phone data persistence and phone file readin
 
 //todo I know wes had this working
-function savetoCSV(){
+function savetoCSV(Filename){
 
 }
 
@@ -11,4 +11,16 @@ function readinCSV(Filename){
 
 function loadCSV(){
 
+}
+
+//todo wes does this look rightish?
+function load_data(data){
+    var resultArray = []
+    for (var i = 0;i< data.length;i++) {
+        var temp_arr = data[i];
+        var a_point = new point(temp_arr[0],temp_arr[1]);
+
+        resultArray.push(a_point);
+    };
+    return resultArray;
 }
